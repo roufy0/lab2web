1d3ccfbe66d68df6a7ecf6197a70d2f911ae2a8d
roufy0 <r-a-h-f-11@hotmail.com>
2026-03-02 11:52:19 +0300

    lab 5

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -7,4 +7,8 @@ path(''
 path('list_books/', views.list_books, name= "books.list_books"),
 path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
 path('aboutus/', views.aboutus, name="books.aboutus"),
+path('html5/links', views.links_page, name="books.html5.links"),
+path('html5/text/formatting', views.formatting_page, name="books.html5.formatting"),
+path('html5/listing', views.listing_page, name="books.html5.listing"),
+path('html5/tables', views.tables_page, name="books.html5.tables"),
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -9,3 +9,15 @@ def viewbook(request, bookId):
     return render(request, 'bookmodule/one_book.html', {'bookId': bookId})
 def aboutus(request):
     return render(request, 'bookmodule/aboutus.html')
+
+def links_page(request):
+    return render(request, 'bookmodule/links.html')
+
+def formatting_page(request):
+    return render(request, 'bookmodule/formatting.html')
+
+def listing_page(request):
+    return render(request, 'bookmodule/listing.html')
+
+def tables_page(request):
+    return render(request, 'bookmodule/tables.html')
