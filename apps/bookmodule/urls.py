57da2819a800d13ddd95a62da0e6781446517618
roufy0 <r-a-h-f-11@hotmail.com>
2026-04-02 10:05:22 +0300

    lab 6

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -11,4 +11,5 @@ path('html5/links', views.links_page, name="books.html5.links"),
 path('html5/text/formatting', views.formatting_page, name="books.html5.formatting"),
 path('html5/listing', views.listing_page, name="books.html5.listing"),
 path('html5/tables', views.tables_page, name="books.html5.tables"),
+path('search/', views.search_books, name="books.search"),
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def __getBooksList():
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+    book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+    return [book1, book2, book3]
+
 def index(request):
     return render(request, "bookmodule/index.html")
 def list_books(request):
@@ -21,3 +27,20 @@ def listing_page(request):
 
 def tables_page(request):
     return render(request, 'bookmodule/tables.html')
+
+def search_books(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+        # now filter
+        books = __getBooksList()
+        newBooks = []
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower(): contained = True
+            if not contained and isAuthor and string in item['author'].lower():contained = True
+            if contained: newBooks.append(item)
+        return render(request, 'bookmodule/bookList.html', {'books':newBooks})
+    else:
+        return render(request, 'bookmodule/search.html')
